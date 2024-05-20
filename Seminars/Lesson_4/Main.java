ee247ff0ae7133237cc2e06a213ed835b749f0bc
SergejMilovanov <milowanow-sergej@mail.ru>
2024-05-20 21:21:22 +0300

    Добавил коды по задачам 4-го семинара

---
+++ Seminars/Lesson_4/Main.java
@@ -0,0 +1,20 @@
+package Seminars.Lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+        mystack stack = new mystack();
+        System.out.println(stack.size());
+        stack.push(1);
+        stack.push(2);
+        stack.push(1);
+        stack.push(1);
+        System.out.println(stack);
+        stack.pop();
+        stack.pop();
+        System.out.println(stack);
+        System.out.println(stack.empty());
+        
+        }
+        
+        }
